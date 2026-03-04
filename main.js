a5ff97434cde9cca37cf0cd43ab8501ca660a3b8
rajatradework-coder <rajatrade.work@gmail.com>
2026-03-04 16:12:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+<div class="footer container-fluid pt-5 px-sm-3 px-md-5 mt-5">
+  <div class="row pt-5">
+    <div class="col-lg-3 col-md-6 mb-5">
+      <img src="/images/logo.png" alt="Raja Trade Logo" style="height: 80px; width: auto; margin-bottom: 20px; background:#fff; border-radius:8px; padding:6px;">
+      <p>RAJA TRADE, Mumbai, is a leading consulting firm in the area of Export Incentives and formalities, active in the marketplace since 1995.</p>
+      <div class="d-flex justify-content-start mt-4">
+        <a class="btn btn-outline-light btn-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
+        <a class="btn btn-outline-light btn-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
+        <a class="btn btn-outline-light btn-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+        <a class="btn btn-outline-light btn-square" href="#"><i class="fab fa-instagram"></i></a>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 mb-5">
+      <h4 class="text-primary mb-4">About Us</h4>
+      <p>RAJA TRADE, Mumbai, is a leading consulting firm in the area of Export Incentives and formalities, active in the marketplace since 1995. Our mission is to provide complex improvement in the level of Government incentives Schemes.</p>
+    </div>
+    <div class="col-lg-3 col-md-6 mb-5">
+      <h4 class="text-primary mb-4">Useful Links</h4>
+      <div class="d-flex flex-column justify-content-start">
+        <a class="text-white mb-2" href="/"><i class="fa fa-angle-right mr-2"></i>Home</a>
+        <a class="text-white mb-2" href="/about"><i class="fa fa-angle-right mr-2"></i>About Us</a>
+        <a class="text-white mb-2" href="/services"><i class="fa fa-angle-right mr-2"></i>Our Services</a>
+        <a class="text-white" href="/contact"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 mb-5">
+      <h4 class="text-primary mb-4">Contact Info</h4>
+      <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Latif House, Office No 24, Sant Tukaram Road, Masjid Bunder(East), Mumbai 400009</p>
+      <p class="mb-2"><i class="fa fa-phone-alt text-primary mr-3"></i>02223482676</p>
+      <p class="mb-2"><i class="fa fa-phone-alt text-primary mr-3"></i>+91-9324093211</p>
+      <p><i class="fa fa-envelope text-primary mr-3"></i>mail@rajatrade.com</p>
+    </div>
+  </div>
+</div>
+<div class="container-fluid py-4 px-sm-3 px-md-5" style="background:#111;">
+  <p class="mb-0 text-center text-white">
+    &copy; <a class="text-primary" href="/">Raja Trade</a>. All Rights Reserved.
+  </p>
+</div>
